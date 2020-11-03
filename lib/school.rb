294dbd153e7b9f24830b67d877a0393a65a5ce2a
JaSON <masteraccounthub@gmail.com>
2020-11-03 07:12:28 -0500

    commit tree 4318b8406ad5c873cd11d79be850cdc4e3f46e08

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,25 @@
-# code here!
+require "pry"
+class School
+    attr_accessor :roster
+    attr_reader :name
+
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def add_student(student_name, grade)
+        roster[grade] ||=[]
+        roster[grade] << student_name
+    end
+
+    def grade(student_grade)
+        roster[student_grade]
+    end
+
+    def sort
+        @roster.each do |grade, students|
+        students.sort!
+        end
+    end
+end
